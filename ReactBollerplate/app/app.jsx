eeb8642e9ae57948ce8ffd652177604dcd9d7702
krish <krishqateste07@gmail.com>
2023-12-03 14:58:12 +0530

    create react Bollerplate

---
+++ ReactBollerplate/app/app.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+// var objOne = {
+//   name : "Krish",
+//   location: "India"
+// };
+//
+// var objTwo = {
+//   age: 19,
+//   ...objOne
+// };
+// console.log(objTwo);
+
+ReactDOM.render(
+<h1>Boilerplate</h1>,
+  document.getElementById('app')
+);
